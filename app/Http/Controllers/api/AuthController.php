f6cee8d29c7b162916dc5ee56fb0795df7e6bc1f
smdedar <thededar@gmail.com>
2020-12-29 04:09:43 +0600

    laravel passport auth

---
+++ app/Http/Controllers/api/AuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    //
+    public function register(Request $request){
+
+        $validatedData = $request->validate([
+            'name' => 'required|max:55',
+            'email' => 'email|required|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        $validatedData['password'] = bcrypt($request->password);
+
+        $user = User::create($validatedData);
+
+        $accessToken = $user->createToken('authToken')->accessToken;
+
+        return response([ 'user' => $user, 'access_token' => $accessToken]);
+    }
+}
